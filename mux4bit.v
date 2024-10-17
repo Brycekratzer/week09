83a533939d0f173def594d0ce5ab722d643a8480
bryce Kratzer <brycekratzer@u.boisestate.edu>
2024-10-17 14:42:40 -0600

    Added Files for P3

---
+++ mux4bit.v
@@ -0,0 +1,18 @@
+module mux4to1 (
+    input A,
+    input B,
+    input C,
+    input D,
+    input [1:0]Sel,
+    output reg Y
+);
+
+    always @(*) begin
+        case(Sel)
+            2'b00: Y = A;
+            2'b01: Y = B;
+            2'b10: Y = C;
+            2'b11: Y = D;
+        endcase
+    end
+endmodule
