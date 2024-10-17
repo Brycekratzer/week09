17f82171eb64aafb88ac53da6e66d01fa8b5abef
bryce Kratzer <brycekratzer@u.boisestate.edu>
2024-10-17 15:16:07 -0600

    memBlock updated

--- memblock8bit.v
+++ memblock8bit.v
@@ -1,9 +1,31 @@
 module memblock8bit(
-    input [7:0]InputData,
+    input [31:0]InputData,
     input E,
-    output [7:0]OutputData,
+    output [31:0]OutputData,
 );
 
+dLatch memblock1(
+    .D(InputData[7:0]),
+    .E(E),
+    .Q(OutputData[7:0])
+);
 
+dLatch memblock2(
+    .D(InputData[15:8]),
+    .E(E),
+    .Q(OutputData[15:8])
+);
+
+dLatch memblock3(
+    .D(InputData[23:16]),
+    .E(E),
+    .Q(OutputData[23:16])
+);
+
+dLatch memblock4(
+    .D(InputData[31:17]),
+    .E(E),
+    .Q(OutputData[31:17])
+);
 
 endmodule
